e970dad379ce7d459f220384df2ca4a09ba103f8
rony1philip <26e4u69@gmail.com>
2024-11-25 13:07:32 +0200

    patient register form to do - validation - anatomy - hide next button when submit

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -23,7 +23,7 @@ import {
   useDisclosure,
   Text,
 } from "@chakra-ui/react";
-
+import Multistep from "../../components/Common/PatientRegisterForm";
 import { Search2Icon } from "@chakra-ui/icons";
 import {
   Link as RouterLink,
@@ -32,7 +32,7 @@ import {
 } from "@tanstack/react-router";
 import Logo from "/assets/images/n.png";
 import useAuth from "../../hooks/useAuth";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export const Route = createFileRoute("/_layout/")({
   component: Dashboard,
 });
@@ -47,7 +47,7 @@ function Dashboard() {
       <Container maxW="full">
         <Box pt={12} m={4}>
           <Text fontFamily="cursive" borderColor="#009688" fontSize="4xl">
-            Hi, {currentUser?.full_name || currentUser?.email} 👋🏼
+            Hi, {currentUser?.full_name || currentUser?.id} 👋🏼
           </Text>
           <Text
             paddingBottom={"20%"}
@@ -95,28 +95,13 @@ function Dashboard() {
           <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader>Register Patient</ModalHeader>
+              
               <ModalCloseButton />
               <ModalBody>
-                <FormControl>
-                  <Text>Patient Name</Text>
-                  <Input type="tel" placeholder="Patient Name" />
-                  <Text>Patient Phon Number</Text>
-                  <Input type="tel" placeholder="Phone number" />
-                  <Text>Patient Id</Text>
-                  <Input type="tel" placeholder="Patient Id" />
-                </FormControl>
+                <Multistep></Multistep>
               </ModalBody>
 
-              <ModalFooter>
-                <Button mr={3} onClick={onClose}>
-                  Close
-                </Button>
-
-                <Link type="submit" as={RouterLink} to="/patient-intake">
-                  <Button variant="primary">Patient Intake</Button>
-                </Link>
-              </ModalFooter>
+              
             </ModalContent>
           </Modal>
         </Box>
