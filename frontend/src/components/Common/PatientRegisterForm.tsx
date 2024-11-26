79b1dafcce68628a095a03599093af2bcce36b7a
rony1philip <26e4u69@gmail.com>
2024-11-26 13:39:00 +0200

    patient register form to do - validation - hide next button when submit

--- frontend/src/components/Common/PatientRegisterForm.tsx
+++ frontend/src/components/Common/PatientRegisterForm.tsx
@@ -163,72 +163,7 @@ const Form2 = () => {
   );
 };
 
-const Form3 = () => {
-  return (
-    <>
-      <Heading w="100%" textAlign={"center"} fontWeight="normal">
-        Social Handles
-      </Heading>
-      <SimpleGrid columns={1} spacing={6}>
-        <FormControl as={GridItem} colSpan={[3, 2]}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: "gray.50",
-            }}
-          >
-            Website
-          </FormLabel>
-          <InputGroup size="sm">
-            <InputLeftAddon
-              bg="gray.50"
-              _dark={{
-                bg: "gray.800",
-              }}
-              color="gray.500"
-              rounded="md"
-            >
-              http://
-            </InputLeftAddon>
-            <Input
-              type="tel"
-              placeholder="www.example.com"
-              focusBorderColor="brand.400"
-              rounded="md"
-            />
-          </InputGroup>
-        </FormControl>
 
-        <FormControl id="email" mt={1}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: "gray.50",
-            }}
-          >
-            About
-          </FormLabel>
-          <Textarea
-            placeholder="you@example.com"
-            rows={3}
-            shadow="sm"
-            focusBorderColor="brand.400"
-            fontSize={{
-              sm: "sm",
-            }}
-          />
-          <FormHelperText>
-            Brief description for your profile. URLs are hyperlinked.
-          </FormHelperText>
-        </FormControl>
-      </SimpleGrid>
-    </>
-  );
-};
 
 export default function PatientRegisterForm() {
   const toast = useToast();
@@ -275,11 +210,12 @@ export default function PatientRegisterForm() {
                 w="7rem"
                 isDisabled={step === 2}
                 onClick={() => {
+                  console.log()
                   setStep(step + 1);
                   if (step === 2) {
                     setProgress(100);
                   } else {
-                    setProgress(progress + 50);
+                    setProgress(progress);
                   }
                 }}
               >
